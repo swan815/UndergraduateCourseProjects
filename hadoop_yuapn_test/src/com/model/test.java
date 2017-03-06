9c6ac6e613627b031ca40cb1ec981e7ff64543ca
Lou Siyuan <30940660@qq.com>
2017-03-06 16:03:35 +0800

    基于hadoop的云盘实现

---
+++ hadoop_yuapn_test/src/com/model/test.java
@@ -0,0 +1,14 @@
+package com.model;
+
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+
+public class test {
+
+	String uri = "hdfs://10.1.14.24:9000/";
+	Configuration conf = new Configuration();
+	//FileSystem fs = FileSystem.get(URI.create(uri), conf);
+
+}
